905ad83df909d200e6e6b49a7f24f0aae5d70a65
SmolyanovGregory <gsm-it@yandex.ru>
2017-12-29 11:55:45 +0300

    Add files via upload

--- HW-06/src/main/java/ru/otus/smolyanov/main/Main.java
+++ HW-06/src/main/java/ru/otus/smolyanov/main/Main.java
@@ -7,39 +7,14 @@ package ru.otus.smolyanov.main;
  */
 
 import ru.otus.smolyanov.atm.*;
-import java.util.Collection;
-
-import java.io.IOException;
-import java.util.LinkedList;
 
 public class Main {
 
-  private void run() throws IOException, NotEnoughMoneyException {
-
-//    Listener listener = new Listener(System.in);
-//    listener.start();
-//    ViewerImpl viewerImpl = new ViewerImpl(System.out);
-//    viewerImpl.showMenu();
-    CashMachine cashMachine = new CashMachineImpl();
-    cashMachine.start();
-
-    Collection<Banknote> pack = new LinkedList<>();
-    pack.add(Banknote.FIVE_HUNDRED);
-    pack.add(Banknote.FIVE);
-    pack.add(Banknote.TEN);
-    pack.add(Banknote.TEN);
-
-    cashMachine.cahsEntry(pack);
-
-    System.out.println("Balance: "+cashMachine.getBalance());
-
-    try {
-      Collection<Banknote> pack2 = cashMachine.cachWithdrawal(400);
-    } catch (ImpossibleToGiveSpecifiedAmountException e) {
-      System.out.println(e.getMessage());
-    }
-
-    System.out.println("Balance: "+cashMachine.getBalance());
+  private void run() {
+    CashMachineShellImpl cashMachineShell = new CashMachineShellImpl(new CashMachineImpl());
+    cashMachineShell.setInputStream(System.in);
+    cashMachineShell.setPrintStream(System.out);
+    cashMachineShell.start();
   }
 
   public static void main(String ... args) {
@@ -47,7 +22,6 @@ public class Main {
       Main main = new Main();
       main.run();
     } catch (Exception e) {
-      System.out.println("!!! Error: "+e.getMessage());
       e.printStackTrace();
     }
   }
